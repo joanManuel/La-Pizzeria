b6ec413fa5d41ecbcaa4ce71f4494b4c2c7adf9e
joanManuel <joan.torres.jt@gmail.com>
2019-06-11 18:11:50 -0500

    Agregando el footer

--- footer.php
+++ footer.php
@@ -1,9 +1,18 @@
     <footer>
         <?php 
-        
+            $args = array(
+                'theme_location' => 'header-menu',
+                'container' => 'nav',
+                'after' => '<span class="separador"> | </span>'
+            );
+            wp_nav_menu($args)
         ?>
+        <div class="ubicacion">
+            <p>8179 Bay Avenue Mountain View, CA 94043</p>
+            <p>Teléfono: +1-92-456-7890</p>
+        </div>
     </footer>
-
+            
     <?php wp_footer(); ?>
 </body>
 </html>
